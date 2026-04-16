893bf23bab1dcb8729cf1b73ef29a638349fbbdb
Prince Verma <theprincepatel007.gmail.com>
2026-04-17 02:38:48 +0530

    backend fix cors final

--- backend/src/routes/productivityRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getProductivityScore } = require("../controllers/productivityController");
-const auth = require("../middleware/authMiddleware");
-
-router.get("/score", auth, getProductivityScore);
-
-module.exports = router;
